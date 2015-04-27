30c39da5d80eae791b3a0af9d639c497c8ea72ae
EJOweb <erik@ejoweb.nl>
2015-04-27 19:43:06 +0200

    keuzes gemaakt...

--- content/content.php
+++ content/content.php
@@ -1,5 +1,21 @@
 <article <?php hybrid_attr( 'post' ); ?>>
 
+	<?php if ( is_singular( 'post' ) || is_archive() || is_home() ) : // If viewing a single post. ?>
+
+	<div class="entry-details">
+		<?php 
+			$category = get_the_category();
+			printf( '<a href="%s" title="%s" class="%s">%s</a>',
+				get_category_link( $category[0]->term_id ),
+				esc_attr( sprintf( __( "View all posts in %s" ), $category[0]->name ) ),
+				"category",
+				$category[0]->cat_name
+			);
+		?>
+	</div>
+
+	<?php endif; // End post-type check. ?>
+
 	<?php if ( is_singular( get_post_type() ) ) : // If viewing a single post. ?>
 
 		<header class="entry-header">
@@ -21,35 +37,6 @@
 	<?php else : // If not viewing a single post. ?>
 
 		<header class="entry-header">
-			<div class="entry-details">
-				<span class="category">
-					<?php 
-						$category = get_the_category();
-						printf( '<a href="%s" title="%s">%s</a>',
-							get_category_link( $category[0]->term_id ),
-							esc_attr( sprintf( __( "View all posts in %s" ), $category[0]->name ) ),
-							$category[0]->cat_name
-						);
-					?>
-				</span>
-				<time <?php hybrid_attr( 'entry-published' ); ?>>
-					<?php 
-						$post_date = get_post_time( 'd-m-Y' );
-						$today = date( 'd-m-Y' );
-						$yesterday = date('d-m-Y',strtotime("-1 days"));
-
-						if ($post_date == $today)
-							echo '<i>Vandaag</i>';
-						elseif ($post_date == $yesterday)
-							echo '<i>Gisteren</i>';
-						else 
-							echo get_the_date() . '';
-					?>
-				</time>
-				<span class="comments">
-					0
-				</span>
-			</div>
 			<h2 <?php hybrid_attr( 'entry-title' ); ?>>
 				<a href="<?php the_permalink(); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a>
 			</h2>
@@ -69,10 +56,31 @@
 					}
 					// $read_more_text = '&raquo;';
 				?>
-				&raquo; <a href="<?php the_permalink(); ?>" rel="bookmark" itemprop="url"><?php echo $read_more_text; ?></a>
+				<!-- &raquo; <a href="<?php the_permalink(); ?>" rel="bookmark" itemprop="url"><?php echo $read_more_text; ?></a> -->
 			</p>
 		</div>
 
 	<?php endif; // End single post check. ?>
+
+	<footer class="entry-footer">
+		<time <?php hybrid_attr( 'entry-published' ); ?>>
+			<?php 
+				$post_date = get_post_time( 'd-m-Y' );
+				$today = date( 'd-m-Y' );
+				$yesterday = date('d-m-Y',strtotime("-1 days"));
+
+				if ($post_date == $today)
+					echo '<i>Vandaag</i>';
+				elseif ($post_date == $yesterday)
+					echo '<i>Gisteren</i>';
+				else 
+					echo get_the_date() . '';
+			?>
+		</time>
+		<span class="comments">
+			0
+		</span>
+		<a href="<?php the_permalink(); ?>" rel="bookmark" itemprop="url"><?php echo $read_more_text; ?></a>
+	</footer>
 
 </article><!-- .entry -->
